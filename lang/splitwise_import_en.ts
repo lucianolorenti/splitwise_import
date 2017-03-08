227ada1d265f604f1e2fe88dc8379de11ffbfe5d
Luciano Lorenti <lucianolorenti@gmail.com>
2017-03-08 17:52:43 -0300

    Translations

---
+++ lang/splitwise_import_en.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>ImportWidget</name>
+    <message>
+        <location filename="../main.py" line="85"/>
+        <source>Abrir archivo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="85"/>
+        <source>Hojas de C&#xc3;&#xa1;lculo (*.ods)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="134"/>
+        <source>No se ha introducido monto para el gasto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="142"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="142"/>
+        <source>Se ha producido un error en la fila</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="167"/>
+        <source>Columna </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="198"/>
+        <source>Archivo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="199"/>
+        <source>Hoja</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="200"/>
+        <source>Grupos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="201"/>
+        <source>Fila Inicial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="202"/>
+        <source>Columna Fecha</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="203"/>
+        <source>Formato de fecha</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="204"/>
+        <source>Fecha por defecto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="205"/>
+        <source>Columna Concepto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="209"/>
+        <source>Importar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ splitwise_import.pro
@@ -0,0 +1,2 @@
+SOURCES = main.py config.py  __init__.py  main.py 
+TRANSLATIONS += lang/splitwise_import_es.ts lang/splitwise_import_en.ts
